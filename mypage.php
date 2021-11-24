1d8da031ca92d69ba66b8d5858a38c6ddfb4ac83
MASATAKA-FUKAYA <mstk_bz.tak.koshi@icloud.com>
2021-11-24 22:22:43 +0900

    fix table(Recruit board)

--- mypage.php
+++ mypage.php
@@ -10,7 +10,7 @@ require('auth.php');
 
 $u_id = $_SESSION['user_id'];
 
-//DBからユーザー情報、レベル情報を取得
+//DBからページに表示する情報を取得
 $dbFormData = getUser($u_id);
 debug('取得したユーザーDBデータ：'. print_r($dbFormData, true));
 
@@ -19,6 +19,11 @@ debug('取得したレベルDBデータ：'. print_r($dbLevelData, true));
 
 $dbTeamData = getMyALLTeam($u_id);
 debug('取得したチームDBデータ：'. print_r($dbTeamData, true));
+
+$dbRecBoardData = getMyRecBoard($u_id);
+//掲示板データは長いのでデバッグ省略
+
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 ?>
 
 <?php
@@ -47,7 +52,7 @@ debug('取得したチームDBデータ：'. print_r($dbTeamData, true));
                     </div><!-- /Topに戻る -->
 
                     <div class="prof-contents-2culum">
-                        <div class="prof-img"><img src="img/user_prof01.jpeg" alt="プロフ画像"></div>
+                        <div class="prof-img"><img src="<?php if(!empty($dbFormData['pic'])){ echo $dbFormData['pic']; }else{ echo 'img/user-icon-default.png'; } ?>" alt="プロフ画像"></div>
                         <table class="prof-table">
                             <tbody>
                                 <tr>
@@ -77,13 +82,34 @@ debug('取得したチームDBデータ：'. print_r($dbTeamData, true));
 
                 <section class="prof-wrapper">
                     <h2 class="prof-title">所属チーム</h2>
-                    <div class="prof-contents-2culum">
-                        <div class="prof-img"><img src="img/team_logo01.png" alt="チーム画像"></div>
-                        <p class="team-name"><a href="teamDetail.php?t_id=1">名古屋ドラゴンズ</a></p>
+
+                        <?php if(!empty($dbTeamData)):?>
+
+                            <?php foreach($dbTeamData as $key => $val): ?>
+                                <div class="prof-contents-2culum">
+                                    <div class="prof-img"><img src="<?php echo $val['pic']; ?>" alt="チーム画像"></div>
+                                    <p class="team-name"><a href="teamDetail.php?t_id=<?php echo $val['id']; ?>"><?php echo $val['team_name']; ?></a></p>
+                                </div>
+                            <?php endforeach; ?>
+
+                        <?php else: ?>
+
+                            <div class="prof-contents-2culum">
+                                <p class="team-name">まだチームに参加していません！</p>
+                            </div>
+
+                        <?php endif; ?>
+
+                    <div class="btn-wrapper-side">
+                        <div class="btn btn-mypage">
+                            <a href="teamEdit.php">新規作成</a>
+                        </div>
+                        <div class="btn btn-mypage">
+                            <a href="teamSearch.php">チーム検索</a>
+                        </div>
                     </div>
-                    <div class="btn btn-mypage">
-                        <a href="teamEdit.php">新規作成</a>
-                    </div>
+
+
                 </section>
 
                 <section class="prof-wrapper">
@@ -97,21 +123,56 @@ debug('取得したチームDBデータ：'. print_r($dbTeamData, true));
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td class="table-common-text">対戦相手</td>
-                                <td class="table-common-text">21.10.04</td>
-                                <td class="table-common-text">来週日曜、練習試合お願いします！</td>
-                            </tr>
-                            <tr>
-                                <td class="table-common-text">メンバー</td>
-                                <td class="table-common-text">21.10.04</td>
-                                <td class="table-common-text">和気あいあい、楽しさ重視のチーム…</td>
-                            </tr>
+                            <?php if($dbRecBoardData['opp']): ?>
+                                <?php foreach($dbRecBoardData['opp'] as $key => $val): ?>
+                                    <tr>
+                                        <td>対戦相手</td>
+                                        <td><?php echo mb_substr($val['update_date'], 0, 10) ; ?></td>
+                                        <td><?php echo $val['title']; ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <tr>
+                                    <td>対戦相手</td>
+                                    <td></td>
+                                    <td>まだ投稿がありません。</td>
+                                </tr>
+                            <?php endif; ?>
+
+                            <?php if($dbRecBoardData['mem']): ?>
+                                <?php foreach($dbRecBoardData['mem'] as $key => $val): ?>
+                                    <tr>
+                                        <td>メンバー</td>
+                                        <td><?php echo mb_substr($val['update_date'], 0, 10) ; ?></td>
+                                        <td><?php echo $val['title']; ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <tr>
+                                    <td>メンバー</td>
+                                    <td></td>
+                                    <td>まだ投稿がありません。</td>
+                                </tr>
+                            <?php endif; ?>
+
                         </tbody>
                     </table>
-                    <div class="btn btn-mypage">
-                        <a href="teamEdit.php">メンバー、対戦相手募集</a>
-                    </div>
+                    <?php if(!empty($dbTeamData[0])): ?>
+                        <div class="btn-wrapper-side">
+                            <div class="btn btn-mypage">
+                                <a href="makeOpponentRecruit.php">対戦相手募集</a>
+                            </div>
+                            <div class="btn btn-mypage">
+                                <a href="makeMemberRecruit.php">メンバー募集</a>
+                            </div>
+                        </div>
+                    <?php else: ?>
+                        <div class="btn btn-mypage">
+                            <a href="teamEdit.php">チーム作成</a>
+                        </div>
+                        <p>※チームのホストになると、対戦相手やメンバーを募集することができます。</p>
+                    <?php endif; ?>
+
                 </section>
 
                 <section class="prof-wrapper">
@@ -142,17 +203,7 @@ debug('取得したチームDBデータ：'. print_r($dbTeamData, true));
                     </div>
                 </section>
 
-                <section class="prof-wrapper">
-                    <h2 class="prof-title">お気に入りチーム一覧</h2>
-                    <div class="prof-contents-2culum">
-                        <div class="prof-img"><img src="img/team_logo01.png" alt="チーム画像"></div>
-                        <p class="team-name"><a href="teamDetail.php">名古屋ドラゴンズ</a></p>
-                    </div>
-                    <div class="prof-contents-2culum">
-                        <div class="prof-img"><img src="img/team_logo02.png" alt="チーム画像"></div>
-                        <p class="team-name"><a href="teamDetail.php">保土ヶ谷ベイスターズ</a></p>
-                    </div>
-                </section>
+                <!-- お気に入りチームを追加するならここに -->
 
             </section>
 
